8e33f1759211c6e7b30e29e9c085a90a07df23f3
ILV <inigolv@gmail.com>
2016-04-14 15:03:22 -0500

    Checkpoint-29 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -9,7 +9,6 @@ function revealPoint (point){
 }
 
 
-var pointsArray = document.getElementsByClassName("point");
 
 var animatePoints = function(points) {
     // 1) We want to loop over every point in the HTML, and reveal it using CSS changes
@@ -17,17 +16,32 @@ var animatePoints = function(points) {
     forEach(points, revealPoint);
 }
 
-window.onload = function() {
-    if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-     }
+$(window).load(function() {
+    if ($(window).height() > 950){
+        animatePoints();
+    }
 
-    var sellingPoints =document.getElementsByClassName("selling-points")[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    var scrollDistance = $(".selling-points").offset().top - $(window).height() + 200;
     
-    window.addEventListener("scroll", function(event) {
- if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
+    
+    
+     $(window).scroll(function(event) {
+         // #4
+         if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
          }
      });
- }
+ });
+     
+      var animatePoints = function() {
+     var revealPoint = function() {
+         // #7
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+     };
+     // #6
+     $.each($('.point'), revealPoint);
+ };
+
